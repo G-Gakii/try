5b560582651a38a33251c6b916b4b1f70cbae9a7
“G-Gakii” <gakiiviolet1@gmail.com>
2024-10-25 09:14:04 +0300

    setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+app.get("/", (req, res) => {
+  res.send("hallo from typescript");
+});
+app.listen(PORT, () => {
+  console.log(`listening at port ${PORT} `);
+});

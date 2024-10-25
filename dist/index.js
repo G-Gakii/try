8d359a6f5e6113828881894dd32e6b8ddef12fc1
“G-Gakii” <gakiiviolet1@gmail.com>
2024-10-25 09:48:18 +0300

    settting

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
-    res.send("hallo from typescript");
+    res.send("hallo from typescript much love");
 });
 app.listen(PORT, () => {
     console.log(`listening at port ${PORT} `);
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
-  res.send("hallo from typescript");
+  res.send("hallo from typescript much love");
 });
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT} `);
